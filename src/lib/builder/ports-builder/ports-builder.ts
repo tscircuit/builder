ea2f24c85542cda1c5df0ef30bb58b665c969f2d
Severin Ibarluzea <seveibar@gmail.com>
2022-10-09 00:59:48 -0700

    fix: refactor portsbuilder

--- src/lib/builder/ports-builder/ports-builder.ts
+++ src/lib/builder/ports-builder/ports-builder.ts
@@ -2,12 +2,18 @@ import * as Type from "lib/types"
 import { Builder } from "lib/types"
 import { Except, Simplify } from "type-fest"
 import { ProjectBuilder } from "../project-builder"
+import { createPortBuilder, PortBuilder } from "./port-builder"
 
 export type PortBuilderCallback = (cb: PortsBuilder) => unknown
 export interface PortsBuilder {
   builder_type: "ports_builder"
   project_builder: ProjectBuilder
   appendChild: (child: Builder) => PortsBuilder
+
+  /**
+   * @deprecated this add function doesn't match the other "add" patterns, will
+   * be refactored and the method signature will change/will be renamed
+   */
   add: ((portName: string, schematicPosition: Type.Point) => PortsBuilder) &
     ((params: {
       name: string
@@ -20,56 +26,63 @@ export interface PortsBuilder {
   build(): Type.AnyElement[]
 }
 
-export const createPortsBuilder = (
+export class PortsBuilderClass implements PortsBuilder {
+  builder_type = "ports_builder" as const
   project_builder: ProjectBuilder
-): PortsBuilder => {
-  const builder: PortsBuilder = {
-    project_builder,
-    builder_type: "ports_builder",
-  } as any
+  source_component_id: string
+  schematic_component_id: string
+  pcb_component_id: string
 
-  const internal = {
-    source_component_id: "",
-    schematic_component_id: "",
-    pcb_component_id: "",
-    ports: [] as Array<{
-      name: string
-      schematic_position: Type.Point
-      facing_direction?: "up" | "down" | "left" | "right"
-    }>,
+  ports: PortBuilder[]
+
+  constructor(project_builder: ProjectBuilder) {
+    this.project_builder = project_builder
+    this.ports = []
   }
 
-  builder.add = (...params) => {
+  appendChild(child) {
+    if (child.builder_type === "port_builder") {
+      this.ports.push(child)
+      return this
+    }
+    throw new Error(`"ports_builder" can only append "port_builder"`)
+  }
+
+  add(...params) {
     if (params.length === 1) {
-      internal.ports.push({
-        schematic_position: params[0].center,
-        ...params[0],
-      })
+      this.ports.push(
+        createPortBuilder(this.project_builder)
+          .setSchematicPosition(params[0].center)
+          .setName(params[0].name)
+          .setSchematicDirection(params[0].facing_direction)
+      )
     } else {
       const [name, schematic_position] = params
-      internal.ports.push({
-        name,
-        schematic_position,
-      })
+      this.ports.push(
+        createPortBuilder(this.project_builder)
+          .setSchematicPosition(schematic_position)
+          .setName(name)
+      )
     }
-    return builder
+    return this
   }
 
-  builder.setSourceComponent = (source_component_id) => {
-    internal.source_component_id = source_component_id
-    return builder
+  setSourceComponent(source_component_id) {
+    this.source_component_id = source_component_id
+    return this
   }
-  builder.setSchematicComponent = (schematic_component_id) => {
-    internal.schematic_component_id = schematic_component_id
-    return builder
+  setSchematicComponent(schematic_component_id) {
+    this.schematic_component_id = schematic_component_id
+    return this
   }
-  builder.setPCBComponent = (pcb_component_id) => {
-    internal.pcb_component_id = pcb_component_id
-    return builder
+  setPCBComponent(pcb_component_id) {
+    this.pcb_component_id = pcb_component_id
+    return this
   }
 
-  builder.build = () => {
-    return internal.ports.flatMap((port) => {
+  build() {
+    const { project_builder } = this
+    return this.ports.flatMap((port) => {
       const source_port_id = project_builder.getId("source_port")
       const schematic_port_id = project_builder.getId("schematic_port")
       const pcb_port_id = project_builder.getId("pcb_port")
@@ -78,14 +91,14 @@ export const createPortsBuilder = (
           type: "source_port",
           name: port.name,
           source_port_id,
-          source_component_id: internal.source_component_id,
+          source_component_id: this.source_component_id,
         } as Type.SourcePort,
         {
           type: "schematic_port",
           schematic_port_id,
           source_port_id,
           center: port.schematic_position,
-          facing_direction: port.facing_direction,
+          facing_direction: port.schematic_direction,
         } as Type.SchematicPort,
         {
           type: "pcb_port",
@@ -95,6 +108,10 @@ export const createPortsBuilder = (
       ] as Type.AnyElement[]
     }) as Type.AnyElement[]
   }
+}
 
-  return builder
+export const createPortsBuilder = (
+  project_builder: ProjectBuilder
+): PortsBuilder => {
+  return new PortsBuilderClass(project_builder)
 }
