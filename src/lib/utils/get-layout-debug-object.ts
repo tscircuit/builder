53143d3ac355a43bdf9c9bbb746814e6b8378cc4
Severin Ibarluzea <seveibar@gmail.com>
2023-07-22 23:23:41 -0700

    fix: handle bad hash and no color

--- src/lib/utils/get-layout-debug-object.ts
+++ src/lib/utils/get-layout-debug-object.ts
@@ -51,6 +51,6 @@ export const getDebugLayoutObject = (lo: any): LayoutDebugObject | null => {
     height,
     title,
     content,
-    bg_color: colors[stringHash((lo as any).type || title) % colors.length][4],
+    bg_color: colors[stringHash((lo as any).type || title) % colors.length]?.[4] ?? "#f00",
   }
 }
