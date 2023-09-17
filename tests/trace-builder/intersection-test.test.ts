fc2ecd9a7f62b38fc1361811c86817351356e6e5
seveibar <seveibar@gmail.com>
2023-09-17 14:42:09 -0700

    add (failing) intersection test

---
+++ tests/trace-builder/intersection-test.test.ts
@@ -0,0 +1,29 @@
+import { createProjectBuilder } from "lib/builder"
+import test from "ava"
+import { logLayout } from "../utils/log-layout"
+
+test("intersection test", async (t) => {
+  const projectBuilder = await createProjectBuilder()
+    .add("diode", (db) =>
+      db
+        .setSourceProperties({
+          name: "D1",
+        })
+        .setSchematicRotation("180deg")
+        .setSchematicCenter(-2, 0.5)
+    )
+    .add("resistor", (rb) =>
+      rb
+        .setSourceProperties({ resistance: "1kohm", name: "R1" })
+        .setSchematicCenter(0, 0)
+    )
+    .add("trace", (tb) => tb.addConnections([".R1 > .right", ".D1 > .left"]))
+    .add("resistor", (rb) =>
+      rb.setSchematicCenter(-1, 0.5).setSchematicRotation("-90deg")
+    )
+
+  const projectBuilderOutput = await projectBuilder.build()
+
+  await logLayout("intersection test", projectBuilderOutput)
+  t.pass()
+})
