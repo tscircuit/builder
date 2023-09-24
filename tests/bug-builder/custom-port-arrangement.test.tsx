8e3f10671ea0d1d8df828dd4ad7142b7feff8f27
Severin Ibarluzea <seveibar@gmail.com>
2023-09-24 14:37:42 -0700

    test

---
+++ tests/bug-builder/custom-port-arrangement.test.tsx
@@ -0,0 +1,34 @@
+import test from "ava"
+import { createProjectBuilder } from "../../src"
+import { logLayout } from "../utils/log-layout"
+
+test("custom-port-arrangement bug", async (t) => {
+  const result = await createProjectBuilder()
+    .add("bug", (bb) =>
+      bb
+        .setSourceProperties({ name: "B1" })
+        .setSchematicProperties({
+          port_arrangement: {
+            left_side: {
+              direction: "down",
+              pins: [1],
+            },
+            bottom_side: {
+              direction: "right",
+              pins: [3],
+            },
+            right_side: {
+              direction: "down",
+              pins: [2, 4],
+            },
+          },
+        })
+        .labelPort(3, "IN")
+        .labelPort(2, "OUT")
+        .setSchematicCenter(8, 3)
+    )
+    .build()
+
+  await logLayout(`custom-port-arrangement bug`, result)
+  t.pass()
+})
