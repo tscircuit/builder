ec4fd9e173d0646c86dc66e093796dae23b647b5
seveibar <seveibar@gmail.com>
2024-03-28 12:50:32 -0700

    fix: add schematic path builder to builder exports

--- src/lib/builder/schematic-symbol-builder/index.ts
+++ src/lib/builder/schematic-symbol-builder/index.ts
@@ -1,13 +1,16 @@
 import { SchematicBoxBuilder } from "./schematic-box-builder"
 import { SchematicLineBuilder } from "./schematic-line-builder"
+import { SchematicPathBuilder } from "./schematic-path-builder"
 import { SchematicTextBuilder } from "./schematic-text-builder"
 
 export * from "./schematic-symbol-builder"
 export * from "./schematic-box-builder"
 export * from "./schematic-line-builder"
 export * from "./schematic-text-builder"
+export * from "./schematic-path-builder"
 
 export type SchematicSymbolPrimitiveBuilder =
   | SchematicBoxBuilder
   | SchematicLineBuilder
   | SchematicTextBuilder
+  | SchematicPathBuilder
