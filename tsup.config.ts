d5b9a35ea1f9abecafc30937cb80a64305fba64b
seveibar <seveibar@gmail.com>
2024-03-27 17:04:55 -0700

    fix: switch to exclusively cjs exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   tsconfig: "./tsconfig.json",
   entry: ["./src/index.ts"],
-  format: ["cjs", "esm"],
+  format: ["cjs"],
   treeshake: true,
   dts: true,
   sourcemap: true,
