9958bca7341fc15b254f94aec65d82e14117592a
seveibar <seveibar@gmail.com>
2024-04-04 14:56:16 -0700

    fix: add zod description for schematic box

--- src/lib/soup/schematic/schematic_box.ts
+++ src/lib/soup/schematic/schematic_box.ts
@@ -1,14 +1,16 @@
 import { z } from "zod"
 import { distance } from "../units"
 
-export const schematic_box = z.object({
-  type: z.literal("schematic_box"),
-  schematic_component_id: z.string(),
-  width: distance,
-  height: distance,
-  x: distance,
-  y: distance,
-})
+export const schematic_box = z
+  .object({
+    type: z.literal("schematic_box"),
+    schematic_component_id: z.string(),
+    width: distance,
+    height: distance,
+    x: distance,
+    y: distance,
+  })
+  .describe("Draws a box on the schematic")
 
 export type SchematicBoxInput = z.input<typeof schematic_box>
 export type SchematicBox = z.infer<typeof schematic_box>
