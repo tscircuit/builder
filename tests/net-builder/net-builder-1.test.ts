eca5d7cc5b1e98e87bd283b6cfe329a255d41338
seveibar <seveibar@gmail.com>
2024-05-09 12:26:36 -0700

    test that source_net is created

--- tests/net-builder/net-builder-1.test.ts
+++ tests/net-builder/net-builder-1.test.ts
@@ -1,4 +1,5 @@
 import test from "ava"
+import { su } from "@tscircuit/soup-util"
 import { getTestFixture } from "tests/fixtures/get-test-fixture"
 
 test("net builder 1", async (t) => {
@@ -9,6 +10,9 @@ test("net builder 1", async (t) => {
     .add("net", (nb) => nb.setProps({ name: "N1" }))
     .add("trace", (tb) => tb.setProps({ from: ".R1 > .right", to: ".N1" }))
     .build()
+
+  const [source_net] = su(soup).source_net.list()
+  t.is(source_net.name, "N1")
 
   logSoup(soup)
 })
