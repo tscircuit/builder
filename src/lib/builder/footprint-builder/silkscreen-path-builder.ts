a082e1a70ab50e1ebd1dcc558f0cb803cf4e7e71
seveibar <seveibar@gmail.com>
2024-05-12 22:11:51 -0700

    silkscreen builders

---
+++ src/lib/builder/footprint-builder/silkscreen-path-builder.ts
@@ -0,0 +1,7 @@
+import { BuilderInterface } from "../builder-interface"
+
+export interface SilkscreenPathBuilder extends BuilderInterface {}
+
+export class SilkscreenPathBuilderClass implements SilkscreenPathBuilder {}
+
+export const createSilkscreenPathBuilder = () => {}
