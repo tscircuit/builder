028f30cccf3cb4b55e3cafd5d963d9d1230fd1a5
Severin Ibarluzea <seveibar@gmail.com>
2024-06-11 22:12:12 -0700

    fix:bounds centering algo (#43)

--- src/lib/utils/find-bounds-and-center.ts
+++ src/lib/utils/find-bounds-and-center.ts
@@ -10,15 +10,15 @@ export const findBoundsAndCenter = (
     .filter(isTruthy)
 
   let minX = debugObjects[0].x
-  let maxX = debugObjects[0].x + debugObjects[0].width
+  let maxX = debugObjects[0].x + debugObjects[0].width / 2
   let minY = debugObjects[0].y
-  let maxY = debugObjects[0].y + debugObjects[0].height
+  let maxY = debugObjects[0].y + debugObjects[0].height / 2
 
   for (const obj of debugObjects.slice(1)) {
     minX = Math.min(minX, obj.x)
-    maxX = Math.max(maxX, obj.x + obj.width)
+    maxX = Math.max(maxX, obj.x + obj.width / 2)
     minY = Math.min(minY, obj.y)
-    maxY = Math.max(maxY, obj.y + obj.height)
+    maxY = Math.max(maxY, obj.y + obj.height / 2)
   }
 
   const width = maxX - minX
