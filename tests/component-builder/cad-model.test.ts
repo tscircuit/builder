9592a0a08e5a98ae06ec4886d642a960a5bb2e23
seveibar <seveibar@gmail.com>
2024-07-03 14:39:21 -0700

    improve cad model test

--- tests/component-builder/cad-model.test.ts
+++ tests/component-builder/cad-model.test.ts
@@ -6,21 +6,32 @@ test("add cad_component when cadModel specified", async (t) => {
   const { pb, logSoup } = await getTestFixture(t)
 
   const soup = await pb
-    .add("resistor", (rb) =>
-      rb
+    .add("board", (bb) =>
+      bb
         .setProps({
-          resistance: 100,
-          name: "R2",
-          rotation: "90deg",
-          cadModel: {
-            stlUrl: "https://example.com/resistor.stl",
-          },
+          width: 10,
+          height: 10,
+          center_x: 0,
+          center_y: 0,
         })
-        .setSchematicCenter(0, 2)
+        .add("resistor", (rb) =>
+          rb
+            .setProps({
+              resistance: 100,
+              name: "R2",
+              rotation: "90deg",
+              cadModel: {
+                stlUrl:
+                  "https://modelcdn.tscircuit.com/easyeda_models/download?uuid=84af7f0f6529479fb6b1c809c61d205f&pn=C95209",
+              },
+            })
+            .setSchematicCenter(0, 2)
+        )
     )
     .build()
 
   t.is(su(soup).cad_component.list().length, 1)
+  t.is(su(soup).pcb_board.list().length, 1)
 
   await logSoup(soup)
   t.pass()
