d79d75218bd4060cb21310cde53ef5b9bfb61739
seveibar <seveibar@gmail.com>
2024-07-14 16:34:35 -0700

    repro no error bug

--- tests/net-builder/net-builder-5.test.ts
+++ tests/net-builder/net-builder-5.test.ts
@@ -6,7 +6,7 @@ import { getTestFixture } from "tests/fixtures/get-test-fixture"
  * This test has R1, R2 connected by a net. The test makes sure that a pcb trace
  * is created between R1 and R2, and not multiple
  */
-test.skip("net builder 5", async (t) => {
+test("net builder 5", async (t) => {
   const { pb, logSoup } = await getTestFixture(t)
 
   const soup = await pb
@@ -28,6 +28,7 @@ test.skip("net builder 5", async (t) => {
         pcbY: 0,
       })
     )
+    .add("component", (cb) => cb.setName("Obstacle").setFootprint("dip32_p1"))
     .add("net", (nb) => nb.setProps({ name: "N1" }))
     .add("trace", (tb) => tb.setProps({ from: ".R1 > .right", to: "net.N1" }))
     .add("trace", (tb) => tb.setProps({ from: ".R2 > .left", to: "net.N1" }))
@@ -36,7 +37,9 @@ test.skip("net builder 5", async (t) => {
   await logSoup(soup)
 
   const pcb_traces = su(soup).pcb_trace.list()
+
   // NOTE: there is a bug where there are two pcb traces created (starting at
   // each port that's in the net)
-  t.is(pcb_traces.length, 1, "should only have one pcb trace")
+  // t.is(pcb_traces.length, 1, "should only have one pcb trace")
+  t.pass()
 })
