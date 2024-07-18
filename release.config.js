8c0a2971cb851d092fb8c1e5dfa8b3a71121dca4
Rishabh Gupta <belikerishabh@gmail.com>
2024-07-19 03:39:44 +0530

    prettier check

--- release.config.js
+++ release.config.js
@@ -1,25 +1,28 @@
 export default {
   branches: ["main"],
   plugins: [
-    ["@semantic-release/commit-analyzer", {
-      "preset": "angular",
-      "releaseRules": [
-        {"type": "docs", "scope": "README", "release": "patch"},
-        {"type": "fix", "release": "patch"},
-        {"type": "chore", "release": "patch"},
-        {"type": "refactor", "release": "patch"},
-        {"type": "style", "release": "patch"},
-        {"type": "test", "release": "patch"},
-        {"type": "feat", "release": "minor"},
-        {"type": "perf", "release": "minor"},
-        {"breaking": true, "release": "major"},
-        {"type": "build", "scope": "deps", "release": "patch"},
-        {"message": "*", "release": "patch"}
-      ],
-      "parserOpts": {
-        "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES"]
-      }
-    }],
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [
+          { type: "docs", scope: "README", release: "patch" },
+          { type: "fix", release: "patch" },
+          { type: "chore", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "style", release: "patch" },
+          { type: "test", release: "patch" },
+          { type: "feat", release: "minor" },
+          { type: "perf", release: "minor" },
+          { breaking: true, release: "major" },
+          { type: "build", scope: "deps", release: "patch" },
+          { message: "*", release: "patch" },
+        ],
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
+        },
+      },
+    ],
     "@semantic-release/release-notes-generator",
     ["@semantic-release/npm", { npmPublish: true }],
     [
