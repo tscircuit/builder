ec3ef517c0dec3f3cc6afa9fb2773b8b3dd8973d
seveibar <seveibar@gmail.com>
2024-07-20 12:00:22 -0700

    add test

--- tests/board-builder/board-builder.test.ts
+++ tests/board-builder/board-builder.test.ts
@@ -1,4 +1,5 @@
 import test from "ava"
+import { su } from "@tscircuit/soup-util"
 import { getTestFixture } from "tests/fixtures/get-test-fixture"
 
 test("[smoke] board builder", async (t) => {
@@ -6,10 +7,13 @@ test("[smoke] board builder", async (t) => {
 
   const soup = await pb
     .add("board", (bb) => {
-      bb.setProps({ width: 100, height: 100, center_x: 0, center_y: 0 })
+      bb.setProps({ width: 100, height: 100, pcbX: 10, pcbY: 5 })
     })
     .build()
 
+  const [pcb_board] = su(soup).pcb_board.list()
+  t.is(pcb_board.center.x, 10)
+  t.is(pcb_board.center.y, 5)
   await logSoup(soup)
   t.pass()
 })
