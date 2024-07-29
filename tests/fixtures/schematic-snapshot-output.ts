7879f6bf630060fab1c998597c2928411f087269
tscircuitbot <tscircuitbot@users.noreply.github.com>
2024-07-29 19:38:25 +0000

    formatbot: Automatically format code

--- tests/fixtures/schematic-snapshot-output.ts
+++ tests/fixtures/schematic-snapshot-output.ts
@@ -8,11 +8,11 @@ export const schematicSnapshotOutput = async (
   circuit: AnySoupElement[]
 ) => {
   const pngBuffer = circuitToPng(circuit, "schematic")
-  const snapshotDir = path.join(
-    path.dirname(__dirname),
-    "__snapshots__"
-  )
+  const snapshotDir = path.join(path.dirname(__dirname), "__snapshots__")
   const fileNameWithoutSpaces = fileName.replaceAll(" ", "-")
-  const snapshotPath = path.join(snapshotDir, `${fileNameWithoutSpaces}.snapshot.png`)
+  const snapshotPath = path.join(
+    snapshotDir,
+    `${fileNameWithoutSpaces}.snapshot.png`
+  )
   await writeFile(snapshotPath, pngBuffer)
 }
